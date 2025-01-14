e84adf835fe1b26214054380f71d0783b40a3db7
Zakaria El Ghabri <zel-ghab@mi-r1-p1.s19.be>
2025-01-14 17:29:28 +0100

    update

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: zel-ghab <zel-ghab@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:04:17 by zel-ghab          #+#    #+#             */
-/*   Updated: 2024/12/07 17:33:50 by zel-ghab         ###   ########.fr       */
+/*   Updated: 2025/01/14 17:26:47 by zel-ghab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	ft_putchar(int c)
 {
-	char a = (char)c;
+	char	a;
+
+	a = (char)c;
 	write(1, &a, 1);
 	return (1);
 }
